bf63e56b635bab6afbafa3586a87a2590e6c5469
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-10-30 14:04:39 +0500

    output of cart items added

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,15 +1,37 @@
-import React from "react";
+import { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = props => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
-        label={`amount_  ${props.id}`}
+        ref={amountInputRef}
+        label="Amount"
         input={{
-          id: "amount",
+          id: "amount_" + props.id,
           type: "number",
           min: "1",
           max: "5",
@@ -17,7 +39,8 @@ const MealItemForm = props => {
           defaultValue: "1",
         }}
       />
-      <button className>+ Add</button>
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,10 @@
-import React from "react";
+import { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import classes from "./Modal.module.css";
 
 const Backdrop = props => {
-  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = props => {
@@ -19,7 +19,7 @@ const portalElement = document.getElementById("overlays");
 
 const Modal = props => {
   return (
-    <>
+    <Fragment>
       {ReactDOM.createPortal(
         <Backdrop onClose={props.onClose} />,
         portalElement
@@ -28,7 +28,7 @@ const Modal = props => {
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
-    </>
+    </Fragment>
   );
 };
 
