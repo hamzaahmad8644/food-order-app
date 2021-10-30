28b3b49855c80c6229626cc5daf28935651413c7
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-10-30 12:27:26 +0500

    Modal Added for cart items with portals

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const MealItemForm = () => {
-  return <div></div>;
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = props => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label={`amount_  ${props.id}`}
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button className>+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
